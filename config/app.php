90568d91881085de002ea2ad20401648dbbe8c40
Paolo Davila <pdavila@iesebre.com>
2016-12-16 17:30:02 +0100

    Sync

--- config/app.php
+++ config/app.php
@@ -201,6 +201,8 @@ return [
         Barryvdh\Debugbar\ServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         #llum_providers
 
     ],
@@ -265,6 +267,7 @@ return [
          * See: https://github.com/acacha/llum
          */
         'AdminLTE' => Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -11,14 +11,10 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 50)->create()->each(function ($user) {
+        factory(App\User::class, 5)->create()->each(function ($user) {
             $user->tasks()->saveMany(
-                factory(App\Task::class, 5)->create(['user_id' => $user->id])
+                factory(App\Task::class, 10)->create(['user_id' => $user->id])
             );
         });
-
-//        factory(App\Task::class,30)->create();
-////        $task = factory(App\Task::class)->make();
-////        factory('App\Task')
     }
 }
