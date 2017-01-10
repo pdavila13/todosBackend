20afc72b70288a0c7214be3bfa0c4913edf23b04
Paolo Davila <pdavila@iesebre.com>
2017-01-10 17:29:26 +0100

    CSS: Inline

--- resources/views/boxmodel.blade.php
+++ resources/views/boxmodel.blade.php
@@ -7,7 +7,33 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
+<style>
+    #bloc1 {
+        width: 600px;
+        margin: 20px auto;
+        border: solid;
+    }
+
+    #bloc2 {
+        width: 600px;
+        margin: 20px auto;
+        padding: 50px;
+        border: solid;
+        border-width: 10px;
+    }
+</style>
+
 <body>
 
+<div id="bloc1">
+    <span>Això es una prova inline</span>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolore dolorem dolorum eum expedita maxime, molestiae nobis nostrum quasi quibusdam sed totam unde vitae. Illum nostrum quidem vero. Neque, tenetur.
+    <a href="http://www.google.com" style="display: block;">Google</a>
+</div>
+
+<div id="bloc2">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolores facere impedit incidunt molestiae non placeat veniam. Distinctio porro recusandae voluptatem! Architecto beatae debitis delectus fuga ipsam iure maxime quod?
+</div>
 </body>
 </html>
