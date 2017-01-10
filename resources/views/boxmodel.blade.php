57881cbb766c2cc5b817471fb53013d974fd5c2c
Paolo Davila <pdavila@iesebre.com>
2017-01-10 17:50:54 +0100

    CSS: Border-box

--- resources/views/boxmodel.blade.php
+++ resources/views/boxmodel.blade.php
@@ -12,28 +12,28 @@
     #bloc1 {
         width: 600px;
         margin: 20px auto;
-        border: solid;
+        border: solid green 10px;
+        box-sizing: border-box;
     }
 
     #bloc2 {
         width: 600px;
         margin: 20px auto;
         padding: 50px;
-        border: solid;
-        border-width: 10px;
+        border: solid red 10px;
+        box-sizing: border-box;
     }
 </style>
 
 <body>
 
 <div id="bloc1">
-    <span>Això es una prova inline</span>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolore dolorem dolorum eum expedita maxime, molestiae nobis nostrum quasi quibusdam sed totam unde vitae. Illum nostrum quidem vero. Neque, tenetur.
-    <a href="http://www.google.com" style="display: block;">Google</a>
 </div>
 
 <div id="bloc2">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolores facere impedit incidunt molestiae non placeat veniam. Distinctio porro recusandae voluptatem! Architecto beatae debitis delectus fuga ipsam iure maxime quod?
 </div>
+
 </body>
 </html>
