a6316451b05b257bed59278df01cb29e77f3107d
Paolo Davila <pdavila@iesebre.com>
2017-01-17 17:13:03 +0100

    Simple flexbox example: OK

--- resources/views/flexboxlayout.blade.php
+++ resources/views/flexboxlayout.blade.php
@@ -7,15 +7,40 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
+<style>
+
+	.header {
+		padding: 10px;
+		margin: 10px;
+		background-color: indianred;
+	}
+
+	.article {
+		padding: 10px;
+		margin: 10px;
+		background-color: dodgerblue;
+	}
+
+	.container {
+		display: flex;
+	}
+
+</style>
+
 <body>
 
 <div class="header">Simple flexbox example</div>
 
-<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cumque deleniti ea est eveniet fugit magni omnis ut velit. Alias error, maxime nisi non nostrum numquam ratione suscipit tenetur!</div>
+<div class="container">
+	<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cumque deleniti ea est eveniet fugit magni omnis ut velit. Alias error, maxime nisi non nostrum numquam ratione suscipit tenetur!</div>
 
-<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cumque deleniti ea est eveniet fugit magni omnis ut velit. Alias error, maxime nisi non nostrum numquam ratione suscipit tenetur!</div>
+	<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cumque deleniti ea est eveniet fugit magni omnis ut velit. Alias error, maxime nisi non nostrum numquam ratione suscipit tenetur!</div>
 
-<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cumque deleniti ea est eveniet fugit magni omnis ut velit. Alias error, maxime nisi non nostrum numquam ratione suscipit tenetur!</div>
+	<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cumque deleniti ea est eveniet fugit magni omnis ut velit. Alias error, maxime nisi non nostrum numquam ratione suscipit tenetur!</div>
+
+	<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cumque deleniti ea est eveniet fugit magni omnis ut velit. Alias error, maxime nisi non nostrum numquam ratione suscipit tenetur!</div>
+</div>
 
 </body>
 </html>
