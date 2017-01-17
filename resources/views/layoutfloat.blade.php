23076cc09e4ad11758bcc3bf9b2ef4fbee583f52
Paolo Davila <pdavila@iesebre.com>
2017-01-17 16:39:28 +0100

    Simple example: clearfix and debug

--- resources/views/layoutfloat.blade.php
+++ resources/views/layoutfloat.blade.php
@@ -7,7 +7,92 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
 
+<style>
+	
+	nav {
+		float: left;
+		width: 200px;
+		height: 100vh;
+	}
+
+	section {
+		margin-left: 30px;
+	}
+
+	.clearfix {
+		overflow: auto;
+	}
+
+	.debug {
+		border: solid red 1px;
+	}
+
+</style>
+
+<body>
+	<div class="clearfix">
+		<nav class="debug">
+			<ul>
+				<li>Link 1</li>
+				<li>Link 2</li>
+				<li>Link 3</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</nav> <!-- end-nav -->
+
+		<section>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti nam obcaecati odio officiis
+			provident reiciendis sapiente temporibus ullam ut. Doloribus dolorum eligendi exercitationem, fugit incidunt
+			iste libero quo suscipit?
+		</section>
+
+		<section>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti nam obcaecati odio officiis
+			provident reiciendis sapiente temporibus ullam ut. Doloribus dolorum eligendi exercitationem, fugit incidunt
+			iste libero quo suscipit?
+		</section>
+
+		<section>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti nam obcaecati odio officiis
+			provident reiciendis sapiente temporibus ullam ut. Doloribus dolorum eligendi exercitationem, fugit incidunt
+			iste libero quo suscipit?
+		</section>
+
+		<section>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti nam obcaecati odio officiis
+			provident reiciendis sapiente temporibus ullam ut. Doloribus dolorum eligendi exercitationem, fugit incidunt
+			iste libero quo suscipit?
+		</section>
+
+		<section>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti nam obcaecati odio officiis
+			provident reiciendis sapiente temporibus ullam ut. Doloribus dolorum eligendi exercitationem, fugit incidunt
+			iste libero quo suscipit?
+		</section>
+
+		<section>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti nam obcaecati odio officiis
+			provident reiciendis sapiente temporibus ullam ut. Doloribus dolorum eligendi exercitationem, fugit incidunt
+			iste libero quo suscipit?
+		</section>
+
+		<section>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti nam obcaecati odio officiis
+			provident reiciendis sapiente temporibus ullam ut. Doloribus dolorum eligendi exercitationem, fugit incidunt
+			iste libero quo suscipit?
+		</section>
+
+		<section>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti nam obcaecati odio officiis
+			provident reiciendis sapiente temporibus ullam ut. Doloribus dolorum eligendi exercitationem, fugit incidunt
+			iste libero quo suscipit?
+		</section>
+	</div> <!-- End-container -->
+
+	<div id="footer" class="debug">
+		@copyright Paolo Dávila Inc.
+	</div> <!-- end-footer -->
 </body>
 </html>
