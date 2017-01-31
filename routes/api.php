e44afbb2fdb8ef5cd8f5f043312830afdd373cc2
Paolo Davila <pdavila@iesebre.com>
2017-01-31 19:49:53 +0100

    Modified API

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,7 @@ Route::group(['prefix' => 'v1','middleware' => ['cors','auth:api']], function ()
     Route::resource('task', 'TasksController');
     Route::resource('user', 'UsersController');
     Route::resource('user.task', 'UserTasksController');
+    Route::get('/user', function () {
+       return Auth::User();
+    });
 });
