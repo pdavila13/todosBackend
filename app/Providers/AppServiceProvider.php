e6104ce519529cceabd2cda12d41a4287df4cb64
Paolo Dávila <pdavila@iesebre.com>
2017-03-13 19:36:59 +0100

    Assign user a role when registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\User;
 use Illuminate\Support\ServiceProvider;
+use Spatie\Permission\Models\Role;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      User::created(function ($user) {
+        $user->assignRole('admin');
+      });
     }
 
     /**
