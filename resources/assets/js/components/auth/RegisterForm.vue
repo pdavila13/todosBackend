c31be7bc7928570709fecc2f9d8f43c7d3732de6
Paolo Dávila <pdavila@iesebre.com>
2017-03-14 16:24:37 +0100

    New component auth/RegisterForm and register component to bootstrap.js

---
+++ resources/assets/js/components/auth/RegisterForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <form method="post">
+        <div class="form-group has-feedback">
+            <input type="text" class="form-control" placeholder="" name="name" value=""/>
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+            <input type="email" class="form-control" placeholder="" name="email" value=""/>
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="" name="password"/>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="" name="password_confirmation"/>
+            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+        </div>
+        <div class="row">
+            <div class="col-xs-1">
+                <label>
+                    <div class="checkbox_register icheck">
+                        <label>
+                            <input type="checkbox" name="terms">
+                        </label>
+                    </div>
+                </label>
+            </div><!-- /.col -->
+            <div class="col-xs-6">
+                <div class="form-group">
+                    <button type="button" class="btn btn-block btn-flat" data-toggle="modal" data-target="#termsModal">I agree to the terms</button>
+                </div>
+            </div><!-- /.col -->
+            <div class="col-xs-4 col-xs-push-1">
+                <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+            </div><!-- /.col -->
+        </div>
+    </form>
+</template>
+
+<script></script>
